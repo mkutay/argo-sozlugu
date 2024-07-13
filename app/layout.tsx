2668631268bc6a999977239ad3e762720ac3bfe2
mkutay <me@mkutay.dev>
2024-07-13 21:26:46 +0300

    some ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import '@/app/globals.css';
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/themeProvider';
+import { NavBar } from '@/components/navBar';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -26,7 +28,17 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex flex-col">
+            <NavBar/>
+            {children}
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/navBar.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import * as React from 'react';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import { ThemeToggle } from '@/components/themeToggle';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { Logo } from '@/components/icons';
+
+export function NavBar() {
+  return (
+    <NavigationMenu className="mx-8 md:mx-16 my-8">
+      <NavigationMenuList>
+        <NavigationMenuItem className="sm:hidden">
+          <NavigationMenuTrigger><HamburgerMenuIcon/></NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="p-4 flex flex-col">
+              <ListItem href="/" className="flex flex-row">
+                <Logo size={24} className="mr-2"/>
+                <div>Argo Sözlüğü</div>
+              </ListItem>
+              <ListItem href="/hakkimizda">
+                Hakkımızda
+              </ListItem>
+              <ListItem href="/kelimeler">
+                Kelime Listesi
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="hidden sm:flex">
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg")}>
+              <Logo className="mr-2"/>
+              Argo Sözlüğü
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="hidden sm:flex">
+          <Link href="/hakkimizda" legacyBehavior passHref>
+            <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-md")}>
+              Hakkımızda
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="hidden sm:flex">
+          <Link href="/kelimeler" legacyBehavior passHref>
+            <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-md")}>
+              Kelime Listesi
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <ThemeToggle/>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = (({ className, children, href, ...props }: { className?: string, children: any, href: string }, ref: { ref: any }) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <Link
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          href={href}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none flex flex-row items-center">{children}</div>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+})
